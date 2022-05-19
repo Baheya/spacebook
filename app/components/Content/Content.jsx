93f805708c55b376aa3a2445401b4b07a4110721
Baheya <khalifa.baheya@gmail.com>
2022-05-19 11:59:32 +0200

    updated image styling when last tab is active to adhere to the design

--- app/components/Content/Content.jsx
+++ app/components/Content/Content.jsx
@@ -5,10 +5,23 @@ export function links() {
   return [...sourceLinks(), { rel: 'stylesheet', href: styles }];
 }
 
-export function Content({ name, content }) {
+export function Content({ name, content, image }) {
   return (
     <article className="content">
-      <img src={content.image.url} alt="" />
+      <div className="content-image-wrapper">
+        <img
+          className="content-image"
+          src={image ? image : content.image.url}
+          alt=""
+        />
+        {image ? (
+          <img
+            className="content-image-magnified"
+            src={content.image.url}
+            alt=""
+          />
+        ) : null}
+      </div>
       <div className="content-description">
         <h2>{name}</h2>
         <p>{content.content}</p>
